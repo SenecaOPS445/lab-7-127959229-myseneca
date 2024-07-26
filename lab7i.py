282ab85d646f0524655fee9e9c4db20261d573e5
Manish Raj Dhakal <139878449+127959229-myseneca@users.noreply.github.com>
2024-07-26 16:26:42 -0400

    Update lab7i.py
    Internet was not working in my VM so I had to paste all my code directly to github.

--- lab7i.py
+++ lab7i.py
@@ -1,1 +1,20 @@
 
+#!/usr/bin/env python3
+# Student ID: mrdhakal
+
+def function1():
+    global schoolName  # Ensure that function1 accesses the global variable
+    schoolName = 'SICT'
+    print('print() in function1 on schoolName:', schoolName)
+
+def function2():
+    global schoolName
+    schoolName = 'SSDO'
+    print('print() in function2 on schoolName:', schoolName)
+
+schoolName = 'Seneca'
+print('print() in main on schoolName:', schoolName)
+function1()
+print('print() in main on schoolName:', schoolName)
+function2()
+print('print() in main on schoolName:', schoolName)
